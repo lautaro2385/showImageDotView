5c7d6f990db8d910478845cfcf70da0991171207
Lautaro <lautaro14@gmail.com>
2017-03-18 14:11:28 -0500

    init

---
+++ app/src/main/java/com/lautaro/showimagedotview/MainActivity.java
@@ -0,0 +1,29 @@
+package com.lautaro.showimagedotview;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int RESULT_LOAD_IMAGE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/lautaro/showimagedotview/Receiver/CoverReceive.java
@@ -0,0 +1,28 @@
+package com.lautaro.showimagedotview.Receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.lautaro.showimagedotview.Activity.ShowImageActivity;
+
+public class CoverReceive extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equalsIgnoreCase("com.flycom.dotmatrix.hall.open")) {
+            Intent i = new Intent(context, ShowImageActivity.class);
+            //i.setClassName("com.lautaro.showimagedotview", "com.lautaro.showimagedotview.Activity.ShowImageActivity");
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            i.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            i.putExtra("Close", true);
+            context.startActivity(i);
+        } else if (intent.getAction().equalsIgnoreCase("com.flycom.dotmatrix.hall.close")) {
+            Intent i = new Intent(context, ShowImageActivity.class);
+            //i.setClassName("com.lautaro.showimagedotview", "com.lautaro.showimagedotview.Activity.ShowImageActivity");
+            i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            i.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            context.startActivity(i);
+        }
+    }
+}
